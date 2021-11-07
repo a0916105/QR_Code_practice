fd19ad7eac22b7bb46db9e9d1abf30cb32af0346
a0916105 <whdofjew@gmail.com>
2021-11-07 22:29:45 +0800

    新建Project

---
+++ app/src/main/java/tw/idv/jew/qr_code_practice/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package tw.idv.jew.qr_code_practice.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
